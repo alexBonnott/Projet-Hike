e1301ab4a0a5dffbaceb62c1802adb4d0f7f97fb
Alex Bonnot <A.bonnot@hotmail.com>
2026-03-12 11:26:05 +0100

    carte

---
+++ script.js
@@ -0,0 +1,42 @@
+
+        // L'id du container, par exemple <div id="map"></div>
+        var map = L.map('map').setView([48.5839, 7.7455], 13);
+		
+        // Plan IGN avec une transparence de 50%
+        var planIGN = L.tileLayer('https://data.geopf.fr/wmts?'+
+            '&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&TILEMATRIXSET=PM'+
+            '&LAYER={ignLayer}&STYLE={style}&FORMAT={format}'+
+            '&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}',
+            {
+	            ignLayer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2',
+	            style: 'normal',
+	            format: 'image/png',
+	            service: 'WMTS',
+                opacity: 1,
+                attribution: 'Carte © IGN/Geoplateforme'
+        });
+
+        planIGN.addTo(map);
+		
+		var streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                attribution: '&copy; OpenStreetMap'
+            });
+
+        var satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles © Esri'
+        });
+
+        var baseMaps = {
+            "PlanIgn": planIGN,
+            "OpenStreetMap": streetMap,
+            "Satellite" : satellite
+        };
+
+        new L.GPX("Sentier_des_grands_crue.GPX", {
+        async: true
+        }).on('loaded', function(e) {
+        map.fitBounds(e.target.getBounds());
+        }).addTo(map);
+
+        L.control.layers(baseMaps).addTo(map);
