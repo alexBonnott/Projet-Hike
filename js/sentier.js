098f3bc495aa47421b3f91caf339639b38b9c9d1
Alex Bonnot <A.bonnot@hotmail.com>
2026-04-27 07:34:44 +0200

    Ajout d'un stystème de swipe pour la page index concernant les dernières randonnées, et un swipe pour les images des randonnées pour mobile. Réorganisation du projet, centralisation d'un fichier unique js paour toute les pages des randonnees, ansi un fichier css centralisée. Sentier.js contient maintenant des fonction en addEventListener ajouté a tous les objet clickable. Plus création d'une partie de chaque page dynamiquement(photo présentation). Nouveau paramètre dans le fichier json : liste des images et liste des icons.

--- js/sentier.js
+++ js/sentier.js
@@ -35,9 +35,17 @@
             "Satellite" : satellite
         };
 
-        //Ajout de parcour gpx
+        L.control.layers(baseMaps).addTo(map);
 
-        new L.GPX("../../gpx/valleedelouche/valleedelouche.gpx", {
+        //Recuperation du sentier
+        const response = await fetch("../../json/sentiers.json");
+        const lSentiers = await response.json();
+        const nom = document.body.dataset.randonnee.toLowerCase();
+        
+        const sentier = lSentiers.filter( s => s.nom.toLowerCase() === nom)[0];
+      
+        //Ajout de parcour gpx
+        new L.GPX(sentier.gpx, {
             async: true,
             polyline_options: {
             color: "blue",
@@ -48,33 +56,10 @@
             }
         }).addTo(map);
 
-        //Ajout variante
-
-        new L.GPX("../../gpx/valleedelouche/cheminvariantevalleedelouche.gpx", {
-            async: true,
-            polyline_options: {
-            color: "red",
-            },
-            markers:{
-                startIcon : null,
-                endIcon : null
-            }
-        }).addTo(map);
-        
-    //Ajout des calques à la carte
+    //Ajout des points sur l'itinéraire
     
-    const points = [
-    {latLng : [47.321565, 4.982513], icon : 1},
-    {latLng : [47.326189, 4.983282], icon : 2},
-    {latLng : [47.333288, 4.971143], icon : 3},
-    {latLng : [47.31949126678269, 4.935713661938761], icon : 4},
-    {latLng : [47.307642, 4.919952], icon : 5},
-    {latLng : [47.317318, 4.912805], icon : 6},
-    {latLng : [47.330467, 4.923389], icon : 7},
-    {latLng : [47.341897, 4.975265], icon : 8},
-    {latLng : [47.324137, 4.999859], icon : 9}
-    ];
-
+    const points = sentier.points;
+    
     points.forEach(p =>{
         L.marker(p.latLng, {
             icon : L.divIcon({
@@ -86,72 +71,82 @@
         }).addTo(map);
     });
 
-    L.control.layers(baseMaps).addTo(map);
-    
-    const nbPhoto = 5;
 
-    const images = [
-        "../../images/valleeouche/image1.jpg",
-        "../../images/valleeouche/image2.jpg",
-        "../../images/valleeouche/image3.jpg",
-        "../../images/valleeouche/image4.jpg",
-        "../../images/valleeouche/image5.jpg",
-        "../../images/valleeouche/image6.jpg",
-    ]
 
+    //Recupération de la liste des images du sentier
+    const images = sentier.lImages;
+
+    //position dans la liste des images
     let index = 0;
 
+    //Création dynamique de la suite des images pour mobile
+    const containerImage = document.getElementById("suite-images");
+
+    for(let i =2; i < images.length; i++){
+        containerImage.innerHTML += `<img class="image" alt="image${i+1}" src=${images[i]} data-index="${i}" />`
+    };
+
+    //Ajout d'un click sur chaque images pour ouvrir l'overlay au bon index
+    document.querySelectorAll(".image").forEach(img => {
+        img.addEventListener("click", e => {
+            document.getElementById("overlay").style.display = "flex";
+            index = parseInt(img.dataset.index);
+            showImage();
+        })
+    });
+
+    //Affichage de l'image sur l'overlay
     function showImage(){
         document.getElementById("overlay-img").src = images[index];
         document.getElementById("nb-photo").innerHTML = "photo : " + (index+1) + "/" + images.length;
     }
 
-    function openImage(img){
-        document.getElementById("overlay").style.display = "flex";
-        index = parseInt(img.dataset.index);
-        showImage();
-    }
-
-    function seeMore(){
+    //Ajout d'un clik sur le svg plus pour afficher plus de photo(index 3)
+    document.querySelector("#plus").addEventListener("click", e =>{
         index = 2;
         document.getElementById("overlay").style.display = "flex";
         showImage();
-    }
+    });
 
-    function switchLeft(){
+    //Ajout click sur le boutton gauche pour swicher d'image dans l'overlay
+    document.querySelector("#buttonLeft").addEventListener("click", e =>{
         if(index != 0) index -= 1;
         else index = (images.length-1);
         showImage();
-    }
+    });
 
-    function switchRight(){
+    //Ajout click sur le boutton droit pour swicher d'image dans l'overlay
+    document.querySelector("#buttonRight").addEventListener("click", e => {
         if(index != (images.length-1)) index += 1;
         else index = 0;
         showImage();
-    }
+    });
 
-
-    function closeImg(){
+    //Ajout d'un click sur le svg croix pour fermé l'overlay
+    document.querySelector("#close").addEventListener("click", e => {
         document.getElementById("overlay").style.display = "none";
         index = 0;
-    }
+    });
+
+
+    //Overlay swipe
 
     let index1 = 0;
 
-    let startX = 0;
+    let oStartX = 0;
 
     document.getElementById("overlay").addEventListener("touchstart", e => {
-        startX = e.touches[0].clientX;
+        oStartX = e.touches[0].clientX;
     });
 
     document.getElementById("overlay").addEventListener("touchend", e => {
         let endX = e.changedTouches[0].clientX;
 
-        if (startX - endX > 50) {
+        if (oStartX - endX > 50) {
             // swipe gauche → image suivante
             index++;
             if (index >= images.length) index = 0;
-        } else if (endX - startX > 50) {
+        } else if (endX - oStartX > 50) {
             // swipe droite → image précédente
             index--;
             if (index < 0) index = images.length - 1;
@@ -160,3 +155,4 @@
         img.src = images[index];
         showImage();
     });
+
